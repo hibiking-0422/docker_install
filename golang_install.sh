35afba5fd241f9326be3b5d67feac71289571154
hibiking-0422 <50702357+hibiking-0422@users.noreply.github.com>
2020-01-05 00:51:49 +0900

    add dcoker-compose

--- golang_install.sh
+++ golang_install.sh
@@ -18,4 +18,11 @@ sudo yum install -y docker-ce docker-ce-cli containerd.io
 #docker config
 sudo systemctl start docker
 sudo systemctl enable docker
+
+#docker-compose install
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.1-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+#version check
 docker --version
+docker-compose --version

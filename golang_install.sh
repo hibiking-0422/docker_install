581c5350f63927542e77f63203487bfd445ab63f
hibiking-0422 <50702357+hibiking-0422@users.noreply.github.com>
2020-03-11 13:02:45 +0900

    ｓｓｓ

--- golang_install.sh
+++ golang_install.sh
@@ -25,6 +25,7 @@ sudo systemctl enable docker
 #docker-compose install
 sudo curl -L https://github.com/docker/compose/releases/download/1.25.1-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 #version check
 docker --version
